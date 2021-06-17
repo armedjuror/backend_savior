b457e50954f2eb94291da1009c27d3b312201911
SakshiKabra55 <72736824+SakshiKabra55@users.noreply.github.com>
2021-06-17 23:40:08 +0530

    Add files via upload

---
+++ routes/posts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('we are on home');
+});
+
+router.post('/registerStudent',require('./studentSignup'));
+router.post('/registerDonor',require('./donorSignup'));
+
+router.get('/studentLogin', require('./studentLogin'));
+router.get('/donorLogin', require('./donorLogin'));
+
+router.get('/parentDetails',require('./parentProfile'));
+router.get('/studentDashboard',require('./studentDashboard'));
+
+module.exports = router;
