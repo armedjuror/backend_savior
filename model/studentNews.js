09f6536612977481d7a16f96dd61f1f7f90b99f6
SakshiKabra55 <72736824+SakshiKabra55@users.noreply.github.com>
2021-06-23 19:34:01 +0530

    Delete studentNews.js

--- model/studentNews.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-let studentNews = new mongoose.Schema({
-    title: String,
-    body: String
-});
-
-module.exports = mongoose.model('studentNews', studentSchema);
