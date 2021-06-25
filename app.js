ec2880651da4d8428b03b2c95a4f7e02ee6a8c1d
PrabhuKGP <prabhu.subbuav@gmail.comgit config --global user.name PrabhuKGPgit config --global user.email prabhu.subbuav@gmail.com>
2021-06-25 15:10:42 +0530

    Added payment,paymentVerfification for razorpay payments, added required routes in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const razorpay = require('razorpay');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app=express();
